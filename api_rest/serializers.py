4f47821f35abcc270d9654c002ca4766065026cc
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2025-01-13 12:47:51 -0300

    feat: validate if nickname already in use

--- api_rest/serializers.py
+++ api_rest/serializers.py
@@ -11,3 +11,8 @@ class UserTaskSerializer(serializers.ModelSerializer):
   class Meta: 
     model = UserTasks
     fields = ['user_nickname', 'user_task']
+    
+  def validate_user_nickname(self, value): 
+    if User.objects.filter(user_nickname=value).exists():
+      raise serializers.ValidationError('Nickname already in use.')
+    return value
